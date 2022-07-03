772f773eadf155afbbd0a06833d0d2cdd5296085
santagram3 <soslimso@naver.com>
2022-07-03 19:44:46 +0900

    2022.07.03 가독성 고침

--- sql예제/Blank_Player.sql
+++ sql예제/Blank_Player.sql
@@ -12,23 +12,23 @@ CREATE SEQUENCE qqnum;
 select * from blank;
 
 insert into blank values (qqnum.nextval , '네가 참 궁금해 그건 너도 마찬가지
-이거면 충분해 쫓고 쫓는 이런 놀이' , 10);
+이거면 충분    해 쫓고 쫓는 이런 놀이' , 14);
 
 insert into blank values (qqnum.nextval , 
-'인간의 욕심은 끝이 없고 같은 실수를 반복하지
-너그럽게 살면서 대박을 노린다면 어림 반푼어치
-우린 운명의 주사위를 던졌고 한치 오차도 용납 못하지' , 18);
+'인간의 욕심 은끝이 없고 같은 실수를 반복하지
+너그럽게 살면서 대박을 노린다면 어림반 푼어 치
+우린 운명의 주사위를 던졌고 한치 오차 도 용납 못하지' , 20);
 
 insert into blank values (qqnum.nextval , '준비하시고 (Go) 쏘세요 (Oh)
-That that I like that (Like that)
-기분 좋아 Babe (Babe)
-흔들어 좌 우 위 아래로 (Sing it)
-One two three to the four (Sing it)' , 14);
+That that I lik e t hat (Like that)
+기분 )좋아 (Babe( (((Babe)
+흔들어( 좌) (우 위) 아(래)로 (Sing it)
+One twot hree to thefo ur (Si ng it)' , 28);
 
 insert into blank values (qqnum.nextval , '이듬해 질 녘 꽃 피는 봄 한여름 밤의 꿈
-가을 타 겨울 내릴 눈 1년 네 번 또다시 봄
-정들었던 내 젊은 날 이제는 안녕
-아름답던 우리의 봄 여름 가을 겨울' , 27);
+가을타겨울 내 릴 눈 1 년 네 번또다시  봄
+정들었던내 젊 은 날 이제  는 안녕
+아름   답던 우리의 봄 여름 가을 겨울' , 31);
 
 insert into blank values (qqnum.nextval , 'Look at you 넌 못 감당해 날
 Ya took off hook
@@ -38,8 +38,8 @@ I got to drink up now 네가 싫다 해도 좋아' , 28);
 
 insert into blank values (qqnum.nextval , '아침엔 모닝콜 필수던 내가
 오늘은 번쩍 번쩍 눈이 떠지는가
-데이트 날이라 그런지 어제 꿈도 좋은 꿈 꿨지
-새 신발을 신고' , 16);
+데이트 날이라 그 런지 어제 꿈도 좋은 꿈  꿨지
+새 신발을 신고' , 18);
 
 insert into blank values (qqnum.nextval , 
 '이제야 목적지를 정했지만 가려한 날 막아서네 난 갈 길이 먼데
@@ -62,11 +62,11 @@ create table players (
 select * from players;
 truncate table players;
 
-insert into players values ('구준표',5,60);
-insert into players values ('팔준표',5,56);
-insert into players values ('장혁',6,67);
-insert into players values ('잔혁',6,70);
-insert into players values ('간나야',10,330);
+insert into players values ('구준표',5,92);
+insert into players values ('팔준표',5,85);
+insert into players values ('장혁',6,115);
+insert into players values ('잔혁',6,160);
+insert into players values ('간나야',8,330);
 commit;
 
 
